6422a757161e0d92644becca484d5a16f6150e59
matthew-devonport <roarsintheeast@gmail.com>
2020-02-02 01:34:23 +1300

    Added js onMouse event

--- client/components/CommissionWork.jsx
+++ client/components/CommissionWork.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 
 class CommissionWork extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+        }
+
+    }
+
+    fade= () => {
+        this.setState({
+            style: {
+                widith: '100%',
+                height: 'auto',
+                backgroundColor: 'black',
+                
+                  
+            }
+        })
+    }
+    
 
 
 
     render() {
         return (
             <React.Fragment>
-            <div className="container">
+              <div id="commissionWork"
+               style={this.state.style}
+              onMouseEnter={this.fade}>
             <img src='IMAGES/Commission-Pics-Logo.jpg' className='commissionPic'></img>
             </div>
+            
             </React.Fragment>
         )
     }
