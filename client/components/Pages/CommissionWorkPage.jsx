85d758fe272a64d6c853e67def94228f8c2d5acf
matthew-devonport <roarsintheeast@gmail.com>
2020-02-03 00:03:15 +1300

    Actually did the flex box right and made it responsive with tablets/phones.

--- client/components/Pages/CommissionWorkPage.jsx
+++ client/components/Pages/CommissionWorkPage.jsx
@@ -13,14 +13,12 @@ class CommissionWorkPage extends React.Component {
     <img src="/IMAGES/Commission-Work-Pics/2-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/3-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/4-1.jpg" className="thumbPics"></img>
-    </div>
-    <div className="picColumn">
     <img src="/IMAGES/Commission-Work-Pics/5-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/6-1.jpg" className="thumbPics"></img>
-    <img src="/IMAGES/Commission-Work-Pics/7-1.jpg" className="thumbPics"></img>
-    <img src="/IMAGES/Commission-Work-Pics/8-1.jpg" className="thumbPics"></img>
     </div>
     <div className="picColumn">
+    <img src="/IMAGES/Commission-Work-Pics/7-1.jpg" className="thumbPics"></img>
+    <img src="/IMAGES/Commission-Work-Pics/8-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/9-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/10-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/11-1.jpg" className="thumbPics"></img>
@@ -31,14 +29,12 @@ class CommissionWorkPage extends React.Component {
     <img src="/IMAGES/Commission-Work-Pics/14-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/15-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/16-1.jpg" className="thumbPics"></img>
-    </div>
-    <div className="picColumn">
     <img src="/IMAGES/Commission-Work-Pics/17-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/18-1.jpg" className="thumbPics"></img>
-    <img src="/IMAGES/Commission-Work-Pics/19-1.jpg" className="thumbPics"></img>
-    <img src="/IMAGES/Commission-Work-Pics/20-1.jpg" className="thumbPics"></img>
     </div>
     <div className="picColumn">
+    <img src="/IMAGES/Commission-Work-Pics/19-1.jpg" className="thumbPics"></img>
+    <img src="/IMAGES/Commission-Work-Pics/20-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/21-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/22-1.jpg" className="thumbPics"></img>
     <img src="/IMAGES/Commission-Work-Pics/23-1.jpg" className="thumbPics"></img>
