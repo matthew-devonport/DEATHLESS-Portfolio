24f4b10e9343de506a8e2a9c37386f353b401a97
matthew-devonport <roarsintheeast@gmail.com>
2020-02-02 16:32:17 +1300

    Hover over link with text working

--- client/components/CommissionWork.jsx
+++ client/components/CommissionWork.jsx
@@ -1,32 +1,14 @@
 import React from 'react'
 
 class CommissionWork extends React.Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-        }
-
-    }
-
-    fade= () => {
-        this.setState({
-            style: {
-                widith: '100%',
-                height: 'auto',
-                backgroundColor: 'black', 
-            }
-        })
-
-    }
+ 
     
     render() {
         return (
             <React.Fragment>
-              <div id="commissionWork"
-               style={this.state.style}
-              onMouseEnter={this.fade}>
+              <div className="commissionPic">
             <img src='IMAGES/Commission-Pics-Logo.jpg' className='commissionPic'></img>   
+           
             </div>
             </React.Fragment>
         )
