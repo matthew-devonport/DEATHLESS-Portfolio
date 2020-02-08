d938be1d7aea81fa037e0713ac8dc79097089e08
matthew-devonport <roarsintheeast@gmail.com>
2020-02-09 08:14:50 +1300

    installed axios for email

--- client/components/Pages/Form.jsx
+++ client/components/Pages/Form.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 
 class Form extends React.Component {
-
+   constructor(props) {
+       super(props);
+       super(props);
+       this.state = {
+         name: '',
+         email: '',
+         message: '' };
+       this.handleChange = this.handleChange.bind(this);
+       this.handleSubmit = this.handleSubmit.bind(this);
+   
+   }
 
 
     render() {
         return (
             <React.Fragment>
           <div>
-                    
+           <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+    <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input type="text" className="form-control" />
+    </div>
+    <div className="form-group">
+        <label htmlFor="deathless@email.com">Email address</label>
+        <input type="email" className="form-control" aria-describedby="emailHelp" />
+    </div>
+    <div className="form-group">
+        <label htmlFor="message">Message</label>
+        <textarea className="form-control" rows="5"></textarea>
+    </div>
+    <button type="submit" className="btn btn-primary">Submit</button>
+</form>         
           </div>
             </React.Fragment>
         )
