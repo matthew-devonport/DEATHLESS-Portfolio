de6d643702f5ed0ea36479575c8aa564e9a7bba4
matthew-devonport <roarsintheeast@gmail.com>
2020-02-09 08:02:38 +1300

    Setup Form

---
+++ client/components/Pages/Form.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Form extends React.Component {
+
+
+
+    render() {
+        return (
+            <React.Fragment>
+          <div>
+                    
+          </div>
+            </React.Fragment>
+        )
+    }
+}
+
+
+export default Form
