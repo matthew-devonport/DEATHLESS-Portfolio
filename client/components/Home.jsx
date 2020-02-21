e52b198b79a3c38a906de48571462484d12b12a5
matthew-devonport <roarsintheeast@gmail.com>
2020-02-22 11:35:57 +1300

    added some actions/reducers and coded in home component

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -9,21 +9,32 @@ import Shop from './Shop'
 
 class Home extends React.Component {
  
+  handleClick = pageNum => {
+    const { dispatch } = this.props
+    dispatch(changePage(pageNum))
+  }
+
 
   render () {
     return (
       <div>
       <Header />
       <h1>PORTFOLIO</h1>
+      <div onClick={() => this.handleClick(1)}>
       <Portfolio />
+      </div>
       <div className='background' id='backgroundMidPic'>
       </div>
       <h1>COMMISSION WORK</h1>
+      <div onClick={() => this.handleClick(2)}>
       <CommissionWork />
+      </div>
       <div className='background' id='backgroundMidPic'>
       </div>
       <h1>DEATHLESS STORE</h1>
+      <div onClick={() => this.handleClick(3)}>
       <Shop />
+      </div>
       <div className='background' id='backgroundBottomPic'> 
       </div>
       <Footer />
---
+++ client/reducers/page.js
@@ -0,0 +1,13 @@
+import {CHANGE_PAGE} from '../actions'
+
+function page (state = 0, action) {
+  switch (action.type) {
+    case CHANGE_PAGE:
+      return action.page
+
+    default:
+      return state
+  }
+}
+
+export default page
