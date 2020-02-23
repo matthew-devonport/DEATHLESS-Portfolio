48e964d6c827afb752a19a9622de3960e336a40a
matthew-devonport <roarsintheeast@gmail.com>
2020-02-24 00:39:10 +1300

    done making the nav icons responsive for all pages but home

--- client/components/ComPageNav.jsx
+++ client/components/ComPageNav.jsx
@@ -19,7 +19,7 @@ class ComPageNav extends React.Component {
     render() {
         return (
             <div>
-<div id="comNav">
+<div className="pageNav">
 <a onClick={() => this.setPopup(this.comNav)}>≡­­­</a>
 </div>
 {this.state.popupIsShowing && <PopUpNav content={this.popupContent} togglePopup={this.togglePopup}/>}
