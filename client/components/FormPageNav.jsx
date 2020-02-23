43ae143495b7001f92d2a3760b7f04cda3a7a452
matthew-devonport <roarsintheeast@gmail.com>
2020-02-23 15:19:22 +1300

    set up nav for form

---
+++ client/components/FormPageNav.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { HashRouter as NavLink, Link } from 'react-router-dom';
+import PopUpNav from './PopUpNav'
+
+
+class FormPageNav extends React.Component {
+    state = {
+        popupIsShowing: false,
+      }
+      
+    popupContent = ''
+
+    togglePopup = () => {
+        this.setState({
+          popupIsShowing: !this.state.popupIsShowing
+        })
+      }   
+ 
+    render() {
+        return (
+            <div>
+<div id="comNav">
+<a onClick={() => this.setPopup(this.comNav)}>≡­­­</a>
+</div>
+{this.state.popupIsShowing && <PopUpNav content={this.popupContent} togglePopup={this.togglePopup}/>}
+</div>
+        )          
+}
+
+setPopup = (content) => {
+    this.togglePopup(),
+      this.popupContent = content
+
+}
+
+comNav = (
+      <div id="comNavLinks">
+      <ul>
+         <div className="gradient"><li><Link to='/'>Home</Link></li></div>
+         <div className="gradient"><li><Link to='/Portfolio'>Portfolio</Link></li></div>
+         <div className="gradient"><li><Link to='/Commission'>Commission</Link></li></div>
+         <div className="gradient"><li><Link to='/Shop'>Shop</Link></li></div>
+         <div className="gradient"><li><Link to='/'>About Me</Link></li></div>
+          </ul>
+          </div>
+)  
+
+}
+export default FormPageNav
--- client/components/Pages/NavFormPage.jsx
+++ client/components/Pages/NavFormPage.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Form from './Form'
 import PagesHeader from './PagesHeader'
 import Footer from '../Footer'
+import FormPageNav from '../FormPageNav'
 
 class NavFormPage extends React.Component {
 
@@ -12,6 +13,7 @@ class NavFormPage extends React.Component {
         return (
             <React.Fragment>
                <div className="pageBackPic">
+                 <FormPageNav />  
                <PagesHeader />
                <p className="formTitle">FILL OUT THE FORM BELOW!<br/> <strong>&darr;</strong></p>
                <Form /> 
