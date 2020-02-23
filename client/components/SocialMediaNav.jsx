c928f56c67e9bdf5e0fe49aba0d49a8e4388e822
matthew-devonport <roarsintheeast@gmail.com>
2020-02-23 23:28:13 +1300

    added instagram logo and done component/css. About to apply component to all pages that need.

---
+++ client/components/SocialMediaNav.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+class SocialMediaNav extends React.Component {
+
+
+
+    render() {
+        return (
+            <React.Fragment>
+
+<a href="https://www.instagram.com/deathless_or_die/" target="_blank">
+<img src="IMAGES/Instagram-Icon.png" id="instaIcon"></img>
+</a>
+
+            </React.Fragment>
+        )
+    }
+}
+
+
+export default SocialMediaNav
+
+
+
+
+
+
+
+
