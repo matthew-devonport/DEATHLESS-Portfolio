14ad97adbdd13444e7d7d63c9d13b40e476dc2dd
matthew-devonport <roarsintheeast@gmail.com>
2020-02-27 00:13:02 +1300

    initaly finished the footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -10,8 +10,6 @@ class Footer extends React.Component {
             <React.Fragment>
           <div id='footer'>
           <footer>
-            <div className="row">
-                <div className="col span-1-of-2">
                   <ul className="footer-nav">
         <div className="gradient"><li><Link to='/'>Home</Link></li></div>
         <div className="gradient"><li><Link to='/Portfolio'>Portfolio</Link></li></div>
@@ -20,16 +18,12 @@ class Footer extends React.Component {
         <div className="gradient"><li><Link to='/Contact'>Contact Me</Link></li></div>
         <div className="gradient"><li><Link to='/About'>About Me</Link></li></div>
                    </ul>
-                </div> 
-            </div>
-            <div className="row">
              <p className="copyrightText">
                 Copyright &copy; 2020 by Deathless Studio. All rights reserved.<br />
                 Website Design by Matthew Devonport
-             </p>
-            </div> 
-        </footer></div>   
-
+             </p> 
+        </footer>
+        </div>  
           
             </React.Fragment>
         )
