1d65127832e6c532dba08c4b5b37b62ea0462cae
matthew-devonport <roarsintheeast@gmail.com>
2020-02-26 23:46:12 +1300

    set up links on footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HashRouter as NavLink, Link } from 'react-router-dom';
 
 class Footer extends React.Component {
 
@@ -12,13 +13,19 @@ class Footer extends React.Component {
             <div className="row">
                 <div className="col span-1-of-2">
                   <ul className="footer-nav">
-                    <li><a href="#">About Me</a></li>
+        <div className="gradient"><li><Link to='/'>Home</Link></li></div>
+        <div className="gradient"><li><Link to='/Portfolio'>Portfolio</Link></li></div>
+         <div className="gradient"><li><Link to='/Commission'>Commission</Link></li></div>
+         <div className="gradient"><li><Link to='/Shop'>Shop</Link></li></div>
+        <div className="gradient"><li><Link to='/Contact'>Contact Me</Link></li></div>
+        <div className="gradient"><li><Link to='/About'>About Me</Link></li></div>
                    </ul>
                 </div> 
             </div>
             <div className="row">
              <p className="copyrightText">
-                Copyright &copy; 2020 by Deathless Studio. All rights reserved.
+                Copyright &copy; 2020 by Deathless Studio. All rights reserved.<br />
+                Website Design by Matthew Devonport
              </p>
             </div> 
         </footer></div>   
