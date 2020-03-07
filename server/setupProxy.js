81a108c04ae4967182bfb27e9bfbc23d9549c100
matthew-devonport <roarsintheeast@gmail.com>
2020-03-07 16:45:31 +1300

    changed proxy target to try work on live site

--- server/setupProxy.js
+++ server/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
 
-    app.user(proxy("/api", { target: "http://localhost:3000"}))
+    app.user(proxy("/api", { target: "https://deathless-studio.netlify.com/"}))
 }
 
