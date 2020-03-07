600a66c51329542ff6cc3e742f33d40d8efadfa2
matthew-devonport <roarsintheeast@gmail.com>
2020-03-07 17:35:58 +1300

    trying to use error code catch to fix

--- server/setupProxy.js
+++ server/setupProxy.js
@@ -5,3 +5,7 @@ module.exports = function(app) {
     app.user(proxy("/api", { target: "https://deathless-studio.netlify.com/#/"}))
 }
 
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+  })
+
