bc20638e8be874d086443e1d158bbbd3ddb85b11
matthew-devonport <roarsintheeast@gmail.com>
2020-03-11 23:15:38 +1300

    changed bio

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -16,10 +16,10 @@ class AboutMe extends React.Component {
               <SocialMediaNav />
           <img src="Images/Deathless-Studio-About-Logo.png" className="centerImg" id="aboutLogo"></img>
          <img src="Images/Matt-Parry.jpg" id="parryOne"></img>
-         <p className="aboutText">Deathless Studio was created by tattooist/illustrator Matt Parry from Napier NZ.<br /><br />
-Matt has worked with multiple brands and bands from all over the globe and also illustrated the skate/thrash style artwork for his own clothing line Deathless or Die.<br /><br />
-Heavily influenced by 80’s skateboard graphics and band flyers/artwork from legends such as Jimbo Phillips, VCJ, pushead and Ric Clayton.<br /><br />
-Currently taking on commission work, fill out the form for more info!<br /><br />
+         <p className="aboutText">Deathless Studio is created by tattooist/illustrator Matt Parry from Napier NZ.<br /><br /><br />
+I have worked with multiple brands and bands from all over the globe. I also illustrate the skate/thrash style artwork for my own clothing line Deathless or Die.<br /><br />
+My designs are heavily influenced by 80’s skateboard graphics and band flyers/artwork from legends such as Jimbo Phillips, VCJ, pushead and Ric Clayton.<br /><br />
+I am currently taking on commission work, fill out the form on the contact me page!<br /><br /><br />
 Thanks for looking!</p>
          
           <Footer />   
