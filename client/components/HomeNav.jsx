98b5045926cec64fef3c85e3b284e8147490c735
Matthew Devonport <51934999+matthew-devonport@users.noreply.github.com>
2020-10-27 21:49:07 +1300

    Update HomeNav.jsx

--- client/components/HomeNav.jsx
+++ client/components/HomeNav.jsx
@@ -39,7 +39,7 @@ comNav = (
          <div className="gradient"><li><Link to='/Portfolio'>Art &amp; Illustrations</Link></li></div>
          <div className="gradient"><li><Link to='/Commission'>Commission</Link></li></div>
          <div className="gradient"><li><Link to='/Apparel'>Apparel</Link></li></div>
-         <div className="gradient"><li><a href="https://deathless-studio.myshopify.com/" target="_blank">Shop</a></li></div>
+         <div className="gradient"><li><a href="https://deathlessordie.bigcartel.com/" target="_blank">Shop</a></li></div>
         <div className="gradient"><li><Link to='/Contact'>Contact Me</Link></li></div>
         <div className="gradient"><li><Link to='/About'>About Me</Link></li></div>
           </ul>
